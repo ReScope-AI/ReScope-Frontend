a9d33fa327ab377a407ea3a2bcc035cb66a525fd
nguyenhuykhai <huykhai.work@gmail.com>
2025-07-13 09:38:15 +0700

    fix: deploy error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,18 +1,13 @@
 'use client';
 
-import * as Sentry from '@sentry/nextjs';
 import NextError from 'next/error';
-import { useEffect } from 'react';
 
 export default function GlobalError({
   error
 }: {
   error: Error & { digest?: string };
 }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
+  console.log(error);
   return (
     <html>
       <body>
